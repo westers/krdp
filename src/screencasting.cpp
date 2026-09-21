1aff4c235411529683925201abdb17babb7de444
westers <steve.westerhouse@origami-analytics.com>
2026-09-21 02:35:12 -0500

    screencast: guard all stream requests after denial

--- src/screencasting.cpp
+++ src/screencasting.cpp
@@ -143,6 +143,9 @@ ScreencastingStream *Screencasting::createWorkspaceStream(Screencasting::CursorM
 
 ScreencastingStream *Screencasting::createRegionStream(QRect g, qreal scale, Screencasting::CursorMode mode)
 {
+    if (!d->isActive()) {
+        return nullptr;
+    }
     auto stream = new ScreencastingStream(this);
     stream->d->init(d->stream_region(g.x(), g.y(), g.width(), g.height(), wl_fixed_from_double(scale), mode));
     stream->d->m_size = g.size();
@@ -151,6 +154,9 @@ ScreencastingStream *Screencasting::createRegionStream(QRect g, qreal scale, Scr
 
 ScreencastingStream *Screencasting::createVirtualMonitorStream(const QString &name, const QSize &resolution, qreal dpr, Screencasting::CursorMode mode)
 {
+    if (!d->isActive()) {
+        return nullptr;
+    }
     auto stream = new ScreencastingStream(this);
     stream->d->init(d->stream_virtual_output(name, resolution.width(), resolution.height(), wl_fixed_from_double(dpr), mode));
     stream->d->m_size = resolution;
